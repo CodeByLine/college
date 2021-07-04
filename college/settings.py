acee1470802ffdd861e0a0641bbf7dac6eca2ab3
Yumei <yumei@YMacBook-Pro.local>
2021-07-04 10:35:06 -0400

    static

--- college/settings.py
+++ college/settings.py
@@ -132,11 +132,17 @@ STATICFILES_DIRS = (
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# MEDIA_URL = '/staticfiles/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/media/')
-os.makedirs(STATIC_TMP, exist_ok=True)
-os.makedirs(STATIC_ROOT, exist_ok=True)
-# os.makedirs(MEDIA_ROOT, exist_ok=True)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
